a779b79b9074c8e86fe9bea44ca4fb11a8a0bc4f
Enzo <enzoceleghini.work@gmail.com>
2024-05-30 13:09:05 +0100

    Consertado bugs de importação, E mudança na tabela para um Flatlist

--- Componentes/Produtos.js
+++ Componentes/Produtos.js
@@ -1,41 +1,30 @@
-import { React } from "react";
-import { View, Text, TouchableHighlight, Image } from "react-native";
+import React from "react";
+import { View, Text, TouchableHighlight, Image, FlatList } from "react-native";
 
-export default function Produtos(){
-    return(
-        <View>
-            <TouchableHighlight>
-                <Image
-                    source={require("./img/suco-laranja.png")}
-                />
-                <Text>Sumo de laranja</Text>
-                <Text>1,00€</Text>
-            </TouchableHighlight>
-            
-            <TouchableHighlight>
-                <Image
-                    source={require("./img/tosta-mista.png")}
-                />
-                <Text>Sumo de laranja</Text>
-                <Text>1,20€</Text>
-            </TouchableHighlight>
-            
-            <TouchableHighlight>
-                <Image
-                    source={require("./img/risoles.jpg")}
-                />
-                <Text>Sumo de laranja</Text>
-                <Text>1,60€</Text>
-            </TouchableHighlight>
-            
-            <TouchableHighlight>
-                <Image
-                    source={require("./img/coxinha.png")}
-                />
-                <Text>Sumo de laranja</Text>
-                <Text>2,00€</Text>
-            </TouchableHighlight>
-            
-        </View>
-    )
+const produtos = [
+    { id: '1', imagem: require("./img/suco-laranja.png"), preco: '1,00€', nome: 'Sumo de laranja' },
+    { id: '2', imagem: require("./img/tosta-mista.png"), preco: '1,20€', nome: 'Tosta Mista' },
+    { id: '3', imagem: require("./img/risoles.jpg"), preco: '1,60€', nome: 'Risoles' },
+    { id: '4', imagem: require("./img/coxinha.png"), preco: '2,00€', nome: 'Coxinha' }
+]
+
+export default function Produtos() {
+    return (
+        <FlatList
+            data={produtos}
+            renderItem={({item}) => {
+                return( 
+                    <TouchableHighlight key={item.id}>
+                        <View style={{ flex: 1, margin: 5 }}>
+                            <Image source={item.imagem} style={{width: '100%', height:100}} />
+                            <Text>{item.preco}</Text>
+                            <Text>{item.nome}</Text>
+                        </View>
+                    </TouchableHighlight>
+                )
+            }}
+            keyExtractor={item => item.id}
+            numColumns={2}
+        />
+    );
 }
